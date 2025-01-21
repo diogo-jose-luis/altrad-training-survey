cdfb5f6d6b67cf7faaf944e7d202409ed8797a15
Diogo Luís <diogo.luis.job@hotmail.com>
2025-01-21 11:28:50 +0100

    Departement table splited from departament page

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,18 +1,9 @@
 import Link from "next/link";
 import React from "react";
+import UserTable from "./UserTable";
 
-interface Departamento{
-    id:number;
-    designacao:string;
-    descricao:string;
-}
 
 const DepartamentosPage = async () => {
-
-    const res = await fetch("http://127.0.0.1:8000/departamentos", {cache : 'no-store'});
-  const departamentos : Departamento[] = await res.json();
-
-  console.log(departamentos);
 
   return (
     <div>
@@ -21,25 +12,7 @@ const DepartamentosPage = async () => {
       <hr></hr>
       <Link href="/">Main Page</Link>
       <hr></hr>
-      <>
-        <table className="table table-bordered my-5">
-            <thead>
-                <tr>
-                    <th>Designação</th>
-                    <th>Descrição</th>
-                </tr>
-            </thead>
-            <tbody>
-            {departamentos?.map(departamento => 
-                <tr key={departamento.id}>
-                    <td>{departamento.designacao}</td>
-                    <td>{departamento.descricao}</td>
-                </tr>
-                 )}
-            </tbody>
-            
-        </table>
-      </>
+      <UserTable />
       <hr></hr>
       <Link href="/users/new">New departamento</Link>
     </div>
