ef57a0d69f48ff40c00d208e1611470738d6fbb3
Diogo Luís <diogo.luis.job@hotmail.com>
2025-03-28 16:36:43 +0100

    Removed prisma and API auth

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,7 +0,0 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import authOptions from "../authOptions";
-
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
--- app/users/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const UserNotFoundPage = () => {
-  return (
-    <div>This user doesn´t exist</div>
-  )
-}
-
-export default UserNotFoundPage
